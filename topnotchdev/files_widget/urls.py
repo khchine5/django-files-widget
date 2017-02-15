0970ff3538ddbb7b65438bb26938fbf0a78f0580
khchine5 <hamzakhchine@gmail.com>
2017-02-15 21:32:35 +0100

    Fix migrations

--- topnotchdev/files_widget/urls.py
+++ topnotchdev/files_widget/urls.py
@@ -1,11 +1,12 @@
 try:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls import url, include
 except:
     from django.conf.urls import patterns, url
+from topnotchdev.files_widget.views import upload, thumbnail_url
 
-from django.conf import settings
+# from django.conf import settings
 
-urlpatterns = patterns("topnotchdev.files_widget.views",
-    url(u'^upload/$', "upload", name="files_widget_upload"),
-    url(u'^thumbnail-url/$', "thumbnail_url", name="files_widget_get_thumbnail_url"),
-)
+urlpatterns = [
+    url(r'^upload/$', upload, name="files_widget_upload"),
+    url(r'^thumbnail-url/$', thumbnail_url, name="files_widget_get_thumbnail_url"),
+]
